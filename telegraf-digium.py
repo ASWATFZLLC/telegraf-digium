6c68da4054bef5a61c77b6cee6f76254f2efeb4f
Gaël D. Barbier <gael@ziwo.io>
2020-06-04 17:00:21 +0200

    Added core script to get infos from Digium API

---
+++ telegraf-digium.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+
+import sys
+import getopt
+import re
+import ssl
+import mechanize
+import cookielib
+import urllib
+import json
+from pprint import pprint
+
+class Digium():
+    def __init__(self, host, port, user, password, ssl_skip_verification=False):
+        self.host = "https://" + host + ":" + port
+        self.user = user
+        self.password = password
+        self.ssl_skip_verification = ssl_skip_verification
+        self.cookiejar = cookielib.LWPCookieJar()
+        self.connect = self.api_connect()
+
+    def api_connect(self):
+        data = {
+            'admin_uid': self.user,
+            'admin_password': self.password
+        }
+        data_str = '&'.join(['%s=%s' % (k,v) for k,v in data.items()])
+
+        if self.ssl_skip_verification is True:
+            try:
+                _create_unverified_https_context = ssl._create_unverified_context
+            except AttributeError:
+                pass
+            else:
+                ssl._create_default_https_context = _create_unverified_https_context
+        req = mechanize.Request("%s/admin/main.html" % self.host, data_str)
+        self.cookiejar.add_cookie_header(req)
+        res = mechanize.urlopen(req)
+        lines = res.read()
+     
+        # Check if logged in
+        if re.search("Welcome,\s+%s" % self.user, lines) is None:
+            print('%s: login incorrect for user %s' % (self.host, self.user))
+            sys.exit(-1)
+        return 0
+
+    def api_request(self, data):
+        data_str = json.dumps(data, separators=(',',':'))
+        req = mechanize.Request("%s/json" % self.host, data_str)
+        res = mechanize.urlopen(req)
+        lines = res.read()
+        response = json.loads(lines)
+        result = json.loads(response['response']['result'])
+        return result
+
+def main(argv):
+
+    ##
+    ## Default parameters
+    ##
+
+    host = '127.0.0.1'
+    port = '443'
+    user = 'admin'
+    password = 'admin'
+    ssl_skip_verification = False
+    help_line = "Usage: monitor.py -h <DIGIUM_HOST> [--port <PORT>] -u <USER> -p <PASSWORD> [--ssl_skip_verification]"
+
+    ##
+    ## Get options
+    ##
+
+    try:
+        opts, args = getopt.getopt(argv, "h:u:p:",["port=", "help", "ssl_skip_verification"])
+    except getopt.GetoptError:
+        print help_line
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == "--help":
+            print help_line
+            sys.exit()
+        elif opt == "-h":
+            host = arg
+        elif opt == "--port":
+            port = arg
+        elif opt == "-u":
+            user = arg
+        elif opt =="-p":
+            password = arg
+        elif opt == "--ssl_skip_verification":
+            ssl_skip_verification = True
+
+    ##
+    ## Run baby run
+    ##
+
+    gw = Digium(host=host, port=port, user=user, password=password, ssl_skip_verification=ssl_skip_verification)
+
+    # Request basic infos
+    data = {
+        "request" : {
+            "method": "statistics.list",
+        }
+    }
+    response = gw.api_request(data)
+    print(response)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
+
